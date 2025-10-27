c9003d890d10d3798c348ea7836b8d008f522471
julianfparodi <julianfacundoparodi@gmail.com>
2025-10-27 19:44:04 -0300

    Clase compra y venta

---
+++ TPC_Equipo-6A/Dominio/Compra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Compra
+    {
+        public int idCompra { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public DateTime FechaRecepcion { get; set; }
+        public int IdProveedor { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public bool Estado { get; set; }
+
+
+
+    }
+}
---
+++ TPC_Equipo-6A/Dominio/Venta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Venta
+    {
+        public int idVenta { get; set; }
+        public DateTime FechaVenta { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public int IdCliente { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public bool Estado { get; set; }
+
+
+    }
+}
