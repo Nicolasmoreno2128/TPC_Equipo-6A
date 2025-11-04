0299fbd67d01996366dde1910052e60d5a3ba6fc
Nicolasmoreno2128 <nicolas.moreno2128@gmail.com>
2025-11-04 00:36:55 -0300

    Modificar Producto
    Se termina de dar la funcionalidad a modificar producto

---
+++ TPC_Equipo-6A/TPC_Equipo-6A/ModificarProducto.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ModificarProducto.aspx.cs" Inherits="TPC_Equipo_6A.ModificarProducto" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:HiddenField ID="hfIdProducto" runat="server" />
+
+    <div class="container d-flex justify-content-center align-items-center" style="min-height: 70vh;">
+    <div class="col-md-4 text-center">
+        <h2 class="mb-4">Modificar Producto</h2>
+
+        <div class="mb-3">
+    <asp:TextBox ID="txtNombreProdm" CssClass="form-control mx-auto w-75" placeholder="Nombre del producto" runat="server" />
+        </div>
+        <div class="mb-3">
+            <asp:TextBox ID="txtDescripcionProdm" CssClass="form-control mx-auto w-75" placeholder="Descripción" runat="server" />
+        </div>
+        <div class="mb-3">
+            <asp:TextBox ID="txtImagenProdm" CssClass="form-control mx-auto w-75" placeholder="Imagen" runat="server" />
+        </div>
+        <div class="mb-3">
+            <asp:TextBox ID="txtPrecioProdm" CssClass="form-control mx-auto w-75" placeholder="Precio Producto" runat="server" />
+        </div>
+        <div class="mb-3">
+            <asp:TextBox ID="txtStockProdm" CssClass="form-control mx-auto w-75" placeholder="Stock" runat="server" />
+        </div>
+    <div>
+        <label>Marca</label>
+        <asp:DropDownList ID="ddlMarcam" runat="server" DataTextField="NombreMarca" CssClass="form-control mx-auto w-75" DataValueField="IdMarca"></asp:DropDownList>
+        <asp:RequiredFieldValidator runat="server" ControlToValidate="ddlMarcam" CssClass="form-control mx-auto w-75" InitialValue="" ErrorMessage="Seleccioná una marca" />
+    </div>
+
+    <div>
+        <label>Categoría</label>
+        <asp:DropDownList ID="ddlCategoriam" runat="server" DataTextField="NombreCategoria" CssClass="form-control mx-auto w-75" DataValueField="IdCategoria"></asp:DropDownList>
+        <asp:RequiredFieldValidator runat="server" ControlToValidate="ddlCategoriam" CssClass="form-control mx-auto w-75" InitialValue="" ErrorMessage="Seleccioná una categoría" />
+    </div>
+
+        <div class="d-flex justify-content-center gap-3 mt-3">
+            <asp:Button Text="Modificar" ID="btnModificarProd" OnClick="btnModificarProd_Click" CssClass="btn btn-dark" runat="server" />
+            <asp:Button Text="Cancelar" ID="btnCancelar" OnClick="btnCancelar_Click" CssClass="btn btn-secondary" runat="server" />
+        </div>
+    </div>
+</div>
+
+
+
+</asp:Content>
---
+++ TPC_Equipo-6A/TPC_Equipo-6A/ModificarProducto.aspx.cs
@@ -0,0 +1,101 @@
+﻿using dominio;
+using negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPC_Equipo_6A
+{
+    public partial class ModificarProducto : System.Web.UI.Page
+    {
+        private readonly ProductoNegocio _productoNegocio = new ProductoNegocio();
+        private readonly MarcaNegocio _marcaNegocio = new MarcaNegocio();
+        private readonly CategoriaNegocio _categoriaNegocio = new CategoriaNegocio();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarMarcas();
+                CargarCategorias();
+
+                // ID por QueryString: ModificarProducto.aspx?id=123
+                var idQS = Request.QueryString["id"];
+                if (!string.IsNullOrEmpty(idQS))
+                {
+                    hfIdProducto.Value = idQS;
+                    CargarProducto(int.Parse(idQS));
+                }
+            }
+        }
+
+        private void CargarMarcas()
+        {
+            var marcas = new MarcaNegocio().ListarMarcas();
+            ddlMarcam.DataSource = marcas;
+            ddlMarcam.DataTextField = "NombreMarca";
+            ddlMarcam.DataValueField = "IdMarca";
+            ddlMarcam.DataBind();
+            ddlMarcam.Items.Insert(0, new ListItem("-- Seleccioná --", ""));
+        }
+
+        private void CargarCategorias()
+        {
+            var categorias = new CategoriaNegocio().ListarCategoria();
+            ddlCategoriam.DataSource = categorias;
+            ddlCategoriam.DataTextField = "NombreCategoria";
+            ddlCategoriam.DataValueField = "IdCategoria";
+            ddlCategoriam.DataBind();
+            ddlCategoriam.Items.Insert(0, new ListItem("-- Seleccioná --", ""));
+        }
+
+        private void CargarProducto(int id)
+        {
+            var nego = new ProductoNegocio();
+            var p = nego.ObtenerPorId(id);
+            if (p == null) return;
+
+            txtNombreProdm.Text = p.NombreProducto;
+            txtDescripcionProdm.Text = p.DescripcionProducto;
+            txtImagenProdm.Text = p.UrlImagen;
+            txtPrecioProdm.Text = p.PrecioProducto.ToString();
+            txtStockProdm.Text = p.Stock.ToString();
+            ddlMarcam.SelectedValue = p.IdMarcaFk.ToString();
+            ddlCategoriam.SelectedValue = p.IdCategoriaFk.ToString();
+        }
+
+        protected void btnModificarProd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var p = new Producto
+                {
+                    IdProducto = int.Parse(hfIdProducto.Value),
+                    NombreProducto = txtNombreProdm.Text,
+                    DescripcionProducto = txtDescripcionProdm.Text,
+                    UrlImagen = txtImagenProdm.Text,
+                    PrecioProducto = decimal.Parse(txtPrecioProdm.Text),
+                    Stock = int.Parse(txtStockProdm.Text),
+                    IdMarcaFk = int.Parse(ddlMarcam.SelectedValue),
+                    IdCategoriaFk = int.Parse(ddlCategoriam.SelectedValue),
+                    Estado = true
+                };
+
+                new ProductoNegocio().Modificar(p);
+                Response.Redirect("Productos.aspx", false);
+            }
+            catch (Exception)
+            {
+                throw; // durante desarrollo, dejalo así para ver el error exacto
+            }
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Productos.aspx", false);
+        }
+
+    }
+}
