db42775367e752dc337250adbce69345d673e9a9
Nicolasmoreno2128 <nicolas.moreno2128@gmail.com>
2025-11-12 20:54:29 -0300

    Validacion en el registro
    Se quita validación en el registro

--- TPC_Equipo-6A/TPC_Equipo-6A/Registro.aspx.cs
+++ TPC_Equipo-6A/TPC_Equipo-6A/Registro.aspx.cs
@@ -13,12 +13,6 @@ namespace TPC_Equipo_6A
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Valida que haya un usuario logueado
-            if (Session["usuario"] == null)
-            {
-                Session.Add("error", "Debes loguearte para ingresar");
-                Response.Redirect("Login");
-            }
 
 
 
