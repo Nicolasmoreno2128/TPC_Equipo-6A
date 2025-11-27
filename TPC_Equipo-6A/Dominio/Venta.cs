196e53e8763b92d0a8ae068a7506737e399da662
jaulingui <jaulingui@hotmail.com>
2025-11-26 21:33:23 -0300

    Ajustes x2

--- TPC_Equipo-6A/Dominio/Venta.cs
+++ TPC_Equipo-6A/Dominio/Venta.cs
@@ -14,13 +14,12 @@ namespace dominio
         public decimal TotalVenta   { get; set; }
         public bool Estado { get; set; }
         public Usuario Usuario { get; set; }
-        public List<DetalleVenta> Detalles { get; set; }
-        public List<Pago> Pagos { get; set; }
+        public List<DetalleVenta> Detalles { get; set; }        
 
         public Venta()
         {
             Detalles = new List<DetalleVenta>();
-            Pagos = new List<Pago>();
+            
         }
     }
 }
